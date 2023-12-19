caddc3b59e198f7608869108080c3d32647257b4
Michael Cruz <michael.cruz@ibm.com>
2023-12-19 13:57:59 -0300

    Criado componente de cards

--- black-hannya-site/src/app/app.module.ts
+++ black-hannya-site/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { ApresentacaoComponent } from './components/apresentacao/apresentacao.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent
+    LogoComponent,
+    ApresentacaoComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
