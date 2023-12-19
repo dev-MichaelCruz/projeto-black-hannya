ae51bc8486da7d7f575e6a497f4800c85c1af5d5
Michael Cruz <michael.cruz@ibm.com>
2023-12-19 15:47:24 -0300

    Finalizada estrutura do site

--- black-hannya-site/src/app/app.module.ts
+++ black-hannya-site/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { ApresentacaoComponent } from './components/apresentacao/apresentacao.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { CarrosselComponent } from './components/carrossel-du/carrossel.component';
+import { InfosComponent } from './components/infos/infos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LogoComponent,
     ApresentacaoComponent,
-    CardsComponent
+    CardsComponent,
+    CarrosselComponent,
+    InfosComponent
   ],
   imports: [
     BrowserModule,
